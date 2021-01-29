702894f3f62de54e938b57728df8889e1732b183
cody brunty <codybrunty@gmail.com>
2021-01-29 14:42:14 -0800

    character controller setup

---
+++ DigDug/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_oTargetTransform;
+    public Vector3 m_oTargetOffsetVector;
+    public float m_nMovementSpeed;
+
+    private void Update() {
+        MoveCamera();
+    }
+
+    private void MoveCamera() {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, m_oTargetTransform.position+m_oTargetOffsetVector, m_nMovementSpeed*Time.deltaTime);
+    }
+}
