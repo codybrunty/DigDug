ce0c3952b42467edef1844dfee41445777c7822a
cody brunty <codybrunty@gmail.com>
2021-01-30 22:35:18 -0800

    Resetup Project with Uniersal RenderPipeline
    Joystick Controller
    Player with animations
    Setup Universal Render Pipeline
    Zelda Toon Shader created
    Basic environment setup

---
+++ ExploringGame/Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour{
+
+    public Animator m_oPlayerAnimator;
+
+    public void SetAnimationState(float x, float y) {
+        if(x==0 && y == 0) {
+            IdleAnimation();
+        }
+        else {
+            RunningAnimation();
+        }
+    }
+
+    public void RunningAnimation() {
+        if (!m_oPlayerAnimator.GetBool("isRunning")) {
+            m_oPlayerAnimator.SetBool("isRunning", true);
+        }
+    }
+
+    public void IdleAnimation() {
+        if (m_oPlayerAnimator.GetBool("isRunning")) {
+            m_oPlayerAnimator.SetBool("isRunning", false);
+        }
+    }
+
+}
---
+++ ExploringGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    public MovementJoystick m_oMovementJoystick;
+    public Camera m_oMainCamera;
+    public float m_nMoveSpeed;
+    public float m_nRotateSpeed;
+    public AnimationStateController m_oAnimationStateController;
+
+
+    private void Update() {
+        Vector3 movementVector = MovementInput();
+        RotateTowardMovementVector(movementVector);
+    }
+    
+    private Vector3 MovementInput() {
+        float m_oHorizontal = m_oMovementJoystick.m_vJoystickVector.x;
+        float m_oVertical = m_oMovementJoystick.m_vJoystickVector.y;
+        m_oAnimationStateController.SetAnimationState(m_oHorizontal, m_oVertical);
+        Vector3 movement = new Vector3(m_oHorizontal, 0f, m_oVertical);
+        transform.Translate(movement * m_nMoveSpeed * Time.deltaTime, Space.World);
+        return movement;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementVector) {
+        movementVector = Quaternion.Euler(0f, m_oMainCamera.gameObject.transform.eulerAngles.y, 0f) * movementVector;
+        if (movementVector.magnitude == 0) { return; } 
+        Quaternion rotation = Quaternion.LookRotation(movementVector);
+        gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, rotation, m_nRotateSpeed);
+    }
+
+}
